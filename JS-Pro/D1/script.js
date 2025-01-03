b8347ba35b01d9dd6361cb96e69c9a5ff9813e9b
MedidhiManeesha <medidhimaneesha@gmail.com>
2025-01-03 22:40:04 +0530

    d1 project

---
+++ JS-Pro/D1/script.js
@@ -0,0 +1,56 @@
+var LocationInfo = {
+    USA: {
+      California: {
+        LosAngeles: ["90001", "90002", "90003"],
+        SanFrancisco: ["94101", "94102", "94103"],
+        SanDiego: ["92101", "92102", "92103"]
+      },
+      Texas: {
+        Houston: ["77001", "77002", "77003"],
+        Dallas: ["75201", "75202", "75203"],
+        Austin: ["73301", "73302", "73303"]
+      },
+      Florida: {
+        Miami: ["33101", "33102", "33103"],
+        Orlando: ["32801", "32802", "32803"],
+        Tampa: ["33601", "33602", "33603"]
+      }
+    },
+    India: {
+      Maharashtra: {
+        Mumbai: ["400001", "400002", "400003"],
+        Pune: ["411001", "411002", "411003"],
+        Nagpur: ["440001", "440002", "440003"]
+      },
+      Karnataka: {
+        Bangalore: ["560001", "560002", "560003"],
+        Mysore: ["570001", "570002", "570003"],
+        Hubli: ["580001", "580002", "580003"]
+      },
+      TamilNadu: {
+        Chennai: ["600001", "600002", "600003"],
+        Coimbatore: ["641001", "641002", "641003"],
+        Madurai: ["625001", "625002", "625003"]
+      }
+    },
+    Canada: {
+      Ontario: {
+        Toronto: ["M5A", "M5B", "M5C"],
+        Ottawa: ["K1A", "K1B", "K1C"],
+        Hamilton: ["L8P", "L8R", "L8S"]
+      },
+      BritishColumbia: {
+        Vancouver: ["V5K", "V5L", "V5M"],
+        Victoria: ["V8P", "V8R", "V8S"],
+        Surrey: ["V3T", "V3V", "V3W"]
+      },
+      Alberta: {
+        Calgary: ["T1Y", "T1Z", "T2A"],
+        Edmonton: ["T5A", "T5B", "T5C"],
+        RedDeer: ["T4N", "T4O", "T4P"]
+      }
+    }
+  };
+  
+
+  
